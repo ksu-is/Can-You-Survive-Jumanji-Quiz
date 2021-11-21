5155e784be4ff5e78b4156304fb43c0bd42f0a80
JanaiH <jhemph14@students.kennesaw.edu>
2021-11-20 23:49:41 -0500

    Main characters image
    Added main characters image to project

--- project rough draft.py
+++ project rough draft.py
@@ -4,47 +4,42 @@ A quiz that asks questions and determines whether the user would survive Jumanji
 #import time and time.sleep() allows there to time between when the next line of code appears
 import time
 import tkinter as tk
-#import tkinter.messagebox as msg
 
-class Game(tk.Tk):
-    def __init__(self, welcome, questions, results):
-        self.welcome = welcome
-        self.questions= questions
-        self.results =results
-    def welcome(self):
-        print("Welcome to the Jumanji Quiz!", "\nThis quiz will test whether you could survive Jumanji.")
-#time.sleep(2)
-#character_choice = input("Which character do you want to play as (Choose a letter)?:\nA)Dr.Smoulder Bravestone\nB)Ruby Roundhouse\nC)Professor Shelly Oberon\nD)Franklin Finbar 'Mouse'\n").upper()
+
+
+print("Welcome to the Jumanji Quiz!", "\nThis quiz will test whether you could survive Jumanji.")
+time.sleep(2)
+character_choice = input("Which character do you want to play as (Choose a letter)?:\nA)Dr.Smoulder Bravestone\nB)Ruby Roundhouse\nC)Professor Shelly Oberon\nD)Franklin Finbar 'Mouse'\n").upper()
 
 #Characters to choose from and their characteristics
-    def characters(self):
-        if character_choice== "A":
-            strengths = "Fearless, Climbing, Speed, Boomerang, Smoldering Intensity"
-            weakness = "None"
-            print("You are Dr.Smoulder Bravestone")
-            print("Strengths:", strengths, "\nWeakness: ", weakness)
-        elif character_choice == "B":
-            strengths = "Karate, T*ai Chi, Aikido, Dance Fighting"
-            weakness = "Venom"
-            print("You are Ruby Roundhouse")
-            print("Strengths:", strengths, "\nWeakness: ", weakness)
-        elif character_choice == "C":
-            strengths = "Cartography, Archaeology, Paleontology"
-            weakness= "Endurance"
-            print("You are Professor Shelly Oberon")
-            print("Strengths:", strengths, "\nWeakness: ", weakness)
-        elif character_choice == "D":
-            strengths= "Zoology, Weapons Valet"
-            weakness= "Cake, Speed, Strength"
-            print("You are Franklin Finbar 'Mouse'")
-            print("Strengths:", strengths, "\nWeakness: ", weakness)       
-#characters(character_choice)
-#time.sleep(4)
-    def quiz(self):
-        print("Now let the quiz begin.", "\nChoose a letter for each question.")
-        time.sleep(1)
-        print("Choose wisely!")
-        time.sleep(2)
+def characters(character_choice):
+    if character_choice== "A":
+        strengths = "Fearless, Climbing, Speed, Boomerang, Smoldering Intensity"
+        weakness = "None"
+        print("You are Dr.Smoulder Bravestone")
+        print("Strengths:", strengths, "\nWeakness: ", weakness)
+    elif character_choice == "B":
+        strengths = "Karate, T*ai Chi, Aikido, Dance Fighting"
+        weakness = "Venom"
+        print("You are Ruby Roundhouse")
+        print("Strengths:", strengths, "\nWeakness: ", weakness)
+    elif character_choice == "C":
+        strengths = "Cartography, Archaeology, Paleontology"
+        weakness= "Endurance"
+        print("You are Professor Shelly Oberon")
+        print("Strengths:", strengths, "\nWeakness: ", weakness)
+    elif character_choice == "D":
+        strengths= "Zoology, Weapons Valet"
+        weakness= "Cake, Speed, Strength"
+        print("You are Franklin Finbar 'Mouse'")
+        print("Strengths:", strengths, "\nWeakness: ", weakness)       
+characters(character_choice)
+time.sleep(4)
+    
+print("Now let the quiz begin.", "\nChoose a letter for each question.")
+time.sleep(1)
+print("Choose wisely!")
+time.sleep(2)
 
 #Quiz questions
 question_1= input("There's a hippo coming up out of the water. What should you do?:\nA)Scream for help\nB)Run and hide\nC)Stay as still as possible\nD)Fight it\n").upper()
--- tkinter testing.py
+++ tkinter testing.py
@@ -1,9 +1,52 @@
 from tkinter import *
+from PIL import ImageTk, Image
+#To use code, you must install Pillow and check to make sure tkinter is on your system
 
+
+print("Welcome to the Jumanji Quiz!", "\nThis quiz will test whether you could survive Jumanji.")
 root = Tk()
+root.title("Can You Survive?: Jumanji Quiz")
+main_img= ImageTk.PhotoImage(Image.open("maincharacters.jpg"))
+my_label= Label(image= main_img)
+my_label.pack()
+button_quit= Button(root, text= "Press Exit and return to quiz in terminal", command= root.quit)
+button_quit.pack()
+mainloop()
+character_choice = input("Which character do you want to play as (Choose a letter)?:\nA)Dr.Smoulder Bravestone\nB)Ruby Roundhouse\nC)Professor Shelly Oberon\nD)Franklin Finbar 'Mouse'\n").upper()
 
-myLabel1= Label(root, text = "hello world")
-myLabel2 = Label(root, text= "my name is janai")
+#Characters to choose from and their characteristics
 
-myLabel1.grid(row= 0, column= 0)
-myLabel2.grid(row=1, column= 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#e= Entry(root, width= 100, borderwidth= 5)
+#e.pack()
+#question_1= "There's a hippo coming up out of the water. What should you do?:\nA)Scream for help\nB)Run and hide\nC)Stay as still as possible\nD)Fight it\n"
+#e.insert(0,question_1)
+
+
+#def next_click():
+    #question= Label(root, text = e.get())
+    #question.pack()
+    
+#myLabel1= Label(root, text = "hello world").grid(row= 0, column= 0)
+#myLabel2 = Label(root, text= "my name is janai").grid(row=1, column= 0)
+#next_button= Button(root, text= "Next", padx= 5, pady= 5, command= next_click)
+#next_button.pack()
+
+#myLabel1.grid(row= 0, column= 0)
+#myLabel2.grid(row=1, column= 0)
+
+#root.mainloop()
