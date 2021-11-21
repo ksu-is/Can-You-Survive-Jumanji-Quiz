221f2d754e92358df37e69a22449c46b4045f56f
JanaiH <jhemph14@students.kennesaw.edu>
2021-11-20 21:56:14 -0500

    Tkinter testing
    Trying out tkinter to see if I want to put it into the project

---
+++ tkinter testing.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+myLabel1= Label(root, text = "hello world")
+myLabel2 = Label(root, text= "my name is janai")
+
+myLabel1.grid(row= 0, column= 0)
+myLabel2.grid(row=1, column= 0)
